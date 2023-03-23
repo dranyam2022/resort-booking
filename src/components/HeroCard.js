5cb337c665dc104279e8cd13741fe2f61aa19683
dranyam2022 <maynardescalante@gmail.com>
2023-03-23 13:40:36 +0800

    done with Hero card styling

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./HeroCard.module.css";
 
 const HeroCard = () => {
-  return <div>HeroCard</div>;
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>STAY WITH COMFORT</h1>
+    </main>
+  );
 };
 
 export default HeroCard;
